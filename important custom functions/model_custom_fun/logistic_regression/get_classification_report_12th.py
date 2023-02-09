a18bba099245cb8b0d05c4bdfbb88bbc882021c8
atif4 <84846589+atif4@users.noreply.github.com>
2023-02-09 11:05:42 +0500

    Delete get_classification_report_12th.py

--- important custom functions/model_custom_fun/logistic_regression/get_classification_report_12th.py
+++
@@ -1,5 +0,0 @@
-def get_classification_report(y_t, y_p):
-    # in classification_report only takes y_test and y_prediction variables to give the report
-    return print(classification_report(y_t, y_p))
-#this is example 
-#get_classification_report(y_test,y_pred)
