2972f4de81940eb1d5ca6c264f120240dfbb4dd3
atif4 <84846589+atif4@users.noreply.github.com>
2023-02-09 11:07:45 +0500

    Delete get_seprate_features_8th.py

--- important custom functions/model_custom_fun/logistic_regression/get_seprate_features_8th.py
+++
@@ -1,6 +0,0 @@
-#Separate Feature and Target Matrix
-def get_seprate_features(df_id,col_id):
-    #col_id is special col or target col b/c this the whole model is going to be created 
-    global X, y
-    X = df_id.drop(col_id,axis = 1) 
-    y = df_id[col_id]
