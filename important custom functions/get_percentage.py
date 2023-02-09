2f4b8e5726917c3f321c24268e1d587b52955842
atif4 <84846589+atif4@users.noreply.github.com>
2023-02-09 10:49:45 +0500

    Delete get_percentage.py

--- important custom functions/get_percentage.py
+++
@@ -1,6 +0,0 @@
-def get_percentage(total_number, col_id, conditional_name):
-    length = len(df[df[col_id]== conditional_name])
-    percentage = (length/total_number)*100
-    result = f"The percentage of {conditional_name} is {percentage}"
-    return result
-get_percentage(1000, 'gender', 'male')
