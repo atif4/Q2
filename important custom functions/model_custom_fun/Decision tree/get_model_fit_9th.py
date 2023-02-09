36ce666040d37a4a59d0123c9e200dcfc43e0bd6
atif4 <84846589+atif4@users.noreply.github.com>
2023-02-09 10:55:58 +0500

    Delete get_model_fit_9th.py

--- important custom functions/model_custom_fun/Decision tree/get_model_fit_9th.py
+++
@@ -1,4 +0,0 @@
-dt = DecisionTreeClassifier()
-def get_model_fit(X_train_id, y_train_id, model_id):
-    #model.fit(only take train variables of X and  y)
-    return model_id.fit(X_train_id,y_train_id)
