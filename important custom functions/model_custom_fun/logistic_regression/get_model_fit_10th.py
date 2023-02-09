05e333eacd476658f29a8d3f6ecd578563eaea4f
atif4 <84846589+atif4@users.noreply.github.com>
2023-02-09 11:06:44 +0500

    Delete get_model_fit_10th.py

--- important custom functions/model_custom_fun/logistic_regression/get_model_fit_10th.py
+++
@@ -1,5 +0,0 @@
-def get_model_fit(X_train_id, y_train_id):
-    #model.fit(only take train variables of X and  y)
-    return logR.fit(X_train_id,y_train_id)
-#this is the example 
-#get_model_fit(X_train,y_train)
