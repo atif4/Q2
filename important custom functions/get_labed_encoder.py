7d98b2d3cb61644fb3b7cda25f72b45f0079b344
atif4 <84846589+atif4@users.noreply.github.com>
2023-02-09 10:49:20 +0500

    Delete get_labed_encoder.py

--- important custom functions/get_labed_encoder.py
+++
@@ -1,7 +0,0 @@
-from sklearn.preprocessing import LabelEncoder
-def get_labed_encoder(col_ids_list):
-    global new_df
-    for col_id_list in col_ids_list:
-        df[col_id_list] = df[col_id_list].apply(LabelEncoder().fit_transform)
-        new_df = df.copy()
-get_labed_encoder([['gender','race/ethnicity', 'parental level of education', 'lunch', 'test preparation course']])
