188fd21776dc3fe9cb918f09f9f14c9f6583dd39
atif4 <84846589+atif4@users.noreply.github.com>
2023-02-09 10:50:05 +0500

    Delete get_scatter_plot.py

--- important custom functions/get_scatter_plot.py
+++
@@ -1,5 +0,0 @@
-def get_scatter_plot(x, y, xlabel, ylabel, title):
-    df.plot(kind = 'scatter',x = x, y = y,alpha = 0.5, color = 'red')
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    plt.title(title)
