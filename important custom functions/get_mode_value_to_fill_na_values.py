9728dfec29982968db0ff623945cde387209515c
atif4 <84846589+atif4@users.noreply.github.com>
2023-02-09 10:49:37 +0500

    Delete get_mode_value_to_fill_na_values.py

--- important custom functions/get_mode_value_to_fill_na_values.py
+++
@@ -1,8 +0,0 @@
-def get_mode_value_to_fill_na_values(col_list):
-    global list_of_mode
-    list_of_mode = []
-    for i in col_list:
-        result = df[i].mode()
-        list_of_mode.append(result)
-    #return list_of_mode
-get_fill_missing_values(missing_col_list)
