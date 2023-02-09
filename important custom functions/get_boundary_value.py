3cffc6ba5bd2f35c062fd7c0f121488c6ffec0ac
atif4 <84846589+atif4@users.noreply.github.com>
2023-02-09 10:48:31 +0500

    Delete get_boundary_value.py

--- important custom functions/get_boundary_value.py
+++
@@ -1,8 +0,0 @@
-def get_boundary_value(list_c_ids):
-    for list_c_id in list_c_ids:
-        print('\n',list_c_id.upper())
-        global  upper_limmite, lower_limmite
-        upper_limmite = df[list_c_id].mean() + 3*df[list_c_id].std()
-        lower_limmite = df[list_c_id].mean() - 3*df[list_c_id].std()
-        print('Highest Allow',list_c_id.upper(),upper_limmite)
-        print('Lowest Allow',list_c_id.upper(),lower_limmite)
