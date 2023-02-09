2e82fb4c29d914ee504a4daeac2092fd24701836
atif4 <84846589+atif4@users.noreply.github.com>
2023-02-09 10:54:53 +0500

    Delete get_confusion_matrix_15th.py

--- important custom functions/model_custom_fun/Decision tree/get_confusion_matrix_15th.py
+++
@@ -1,3 +0,0 @@
-def get_confusion_matrix(y_test_id, y_pred_id):
-    # in confusion_matrix_plot only takes y_test and y_prediction variables to get the metrix
-    return confusion_matrix(y_test_id,y_pred_id)
