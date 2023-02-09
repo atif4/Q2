f56ae0a4b154f995b109660d14f055d41cb9eb39
atif4 <84846589+atif4@users.noreply.github.com>
2023-02-09 10:48:47 +0500

    Delete get_convert_dtype.py

--- important custom functions/get_convert_dtype.py
+++
@@ -1,4 +0,0 @@
-def get_convert_dtype(list_of_c_ids):
-    for list_of_c_id in list_of_c_ids:
-        df[list_of_c_id] = df[list_of_c_id].astype("int64")
-get_convert_dtype([['income','age','loan']])
