742fe8c341301abc7c4b130edd9357f474675112
atif4 <84846589+atif4@users.noreply.github.com>
2023-02-09 10:54:32 +0500

    Delete get_accuracy_score_11th.py

--- important custom functions/model_custom_fun/Decision tree/get_accuracy_score_11th.py
+++
@@ -1,6 +0,0 @@
-def get_accuracy_score(model):
-    global y_pred
-    y_pred = model.predict(X_test)
-    test_accuracy=accuracy_score(y_test,y_pred)*100
-    test_accuracy
-    print("Accuracy for our testing dataset with tuning is : {:.2f}%".format(test_accuracy) )
