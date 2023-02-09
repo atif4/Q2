5f3f9fe583a249c44b2e62e7536a31f8d8f56a30
atif4 <84846589+atif4@users.noreply.github.com>
2023-02-09 11:05:10 +0500

    Delete get_countplot_2nd.py

--- important custom functions/model_custom_fun/logistic_regression/get_countplot_2nd.py
+++
@@ -1,6 +0,0 @@
-#viz
-def get_countplot(col_id, df_id,hu= "gender",pal="mako_r",figsize=(6,4)):
-    #the parameter in which values are can be change at any time
-    plt.figure(figsize=figsize)
-    sns.countplot(x=col_id, data=df_id,hue=df_id[hu].sort_values(),palette=pal)
-    plt.show()
