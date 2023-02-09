88f10b62ec9055295c67fe2714c57e44eac477b6
atif4 <84846589+atif4@users.noreply.github.com>
2023-02-09 10:46:07 +0500

    Delete change a value of col with mean value.py

--- important custom functions/change a value of col with mean value.py
+++
@@ -1,6 +0,0 @@
-def get_mean(c_ids):
-    for c_id in c_ids:
-        df[c_id] = df[c_id].replace(0,np.NaN)
-        mean = df[c_id].mean(skipna=True)
-        df[c_id] =df[c_id].replace(np.NaN, mean)
-get_mean([['Glucose', 'BloodPressure', 'SkinThickness', 'Insulin']])
